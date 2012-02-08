50a357946829ea9b4273ddc2ff0c035f281b4449
Benedikt Boss <vork.android@gmail.com>
2012-02-08 19:06:47 +0100

    Update fingerprint

--- cm.mk
+++ cm.mk
@@ -7,7 +7,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lge_star BUILD_ID=GRJ22 BUILD_FINGERPRINT=lge/lge_star/p990_EUR-xx:2.3.4/GRJ22/lgp990-V20l.422C563E:user/release-keys PRIVATE_BUILD_DESC="lge_star-user 2.3.4 GRJ22 422C563E release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lge_star BUILD_ID=MR1 BUILD_FINGERPRINT=generic/cm_p990/p990:4.0.3/MR1/eng.rmcc.20120118.232349:userdebug/test-keys PRIVATE_BUILD_DESC="cm_p990-userdebug 4.0.3 MR1 eng.rmcc.20120118.232349 test-keys"
 
 PRODUCT_NAME := cm_p990
 PRODUCT_BRAND := lge
